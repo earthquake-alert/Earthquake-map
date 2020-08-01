a0a6938672d7bbc6e345e3cb06caa4992114ebc9
Yuto Watanabe <yuto.w51942@gmail.com>
2020-08-01 14:09:19 +0900

    世界地図描画を可能にした

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,25 +4,50 @@ import VectorTileLayer from 'ol/layer/VectorTile';
 import VectorTileSource from 'ol/source/VectorTile';
 import MVTFormat from 'ol/format/MVT';
 import { fromLonLat } from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
 
 import 'ol/ol.css';
+import { VectorTile } from 'ol';
 
-// ベクトルタイルの olレイヤを定義
-const vt = new VectorTileLayer({
+const prefMap = new VectorTileLayer({
   source: new VectorTileSource({
     format: new MVTFormat(),
-    url: 'https://cyberjapandata.gsi.go.jp/xyz/experimental_bvmap/{z}/{x}/{y}.pbf',
-    attributions: [
-      'https://github.com/gsi-cyberjapan/gsimaps-vector-experiment" target="_blank" rel=”noopener noreferrer”>国土地理院',
-    ],
-  })
+    url: 'static/pref/{z}/{x}/{y}.pbf',
+  }),
+  maxZoom: 10,
+  minZoom: 0,
+  maxResolution: 3000,
 });
 
-new Map({
+const distlictMap = new VectorTileLayer({
+  source: new VectorTileSource({
+    format: new MVTFormat({}),
+    url: 'static/distlict/{z}/{x}/{y}.pbf',
+  }),
+  maxZoom: 10,
+  minZoom: 0,
+  maxResolution: 3000,
+});
+
+const worldMao = new VectorTileLayer({
+  source: new VectorTileSource({
+    format: new MVTFormat(),
+    url: 'static/world/{z}/{x}/{y}.pbf',
+  }),
+  maxZoom: 5,
+  minZoom: 0,
+  minResolution: 3000,
+})
+
+const map = new Map({
   target: 'map',
   view: new View({
     center: fromLonLat([140.46, 36.37]),
-    zoom: 10
+    zoom: 6,
   }),
-  layers: [vt]
+  layers: [
+    prefMap,
+    distlictMap,
+    worldMao
+  ],
 });
